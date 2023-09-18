5ebad6ac01761d1b967d75063e2b3627ac11e8e2
Igorsml <igorsmlff@gmail.com>
2023-09-18 13:18:48 +0300

    feat: promise debt 1

--- debts/PromiseSetTimeout/main.js
+++ debts/PromiseSetTimeout/main.js
@@ -3,12 +3,12 @@ const promise = new Promise((resolve, reject) => {
 });
 
 promise
-  .then(() => console.log(1), 1e3)
-  .then(() => console.log(2), 1e3)
-  .then(() => console.log(3), 1e3);
+  .then(() => console.log(1), 1000)
+  .then(() => console.log(2), 1600)
+  .then(() => console.log(3));
 
-promise.then(() => console.log(4), 1e3).then(() => console.log(5), 1e3);
+promise.then(() => console.log(4), 1200).then(() => console.log(5));
 
-promise.then(() => console.log(6), 1e3);
+promise.then(() => console.log(6), 1400);
 
 // 1, 4, 6, 2, 5, 3
