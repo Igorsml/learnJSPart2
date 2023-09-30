ccb38f60182c549cadc39829a169595ac5d9e85e
Igorsml <igorsmlff@gmail.com>
2023-09-30 21:17:24 +0300

    feat: useToggle

--- debts/PromiseSetTimeout/main.js
+++ debts/PromiseSetTimeout/main.js
@@ -3,12 +3,12 @@ const promise = new Promise((resolve, reject) => {
 });
 
 promise
-  .then(() => console.log(1), 1000)
-  .then(() => console.log(2), 1600)
+  .then(() => console.log(1))
+  .then(() => setTimeout(() => console.log(2), 1600))
   .then(() => console.log(3));
 
-promise.then(() => console.log(4), 1200).then(() => console.log(5));
+promise.then(() => console.log(4)).then(() => console.log(5));
 
-promise.then(() => console.log(6), 1400);
+promise.then(() => console.log(6));
 
 // 1, 4, 6, 2, 5, 3
